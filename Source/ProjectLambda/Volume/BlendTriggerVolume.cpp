83f2445644b9bce69d1201df824a0833bf357aab
Daniel-M40 <daniel.moir0993@gmail.com>
2023-11-04 09:57:40 +0000

    Created a trigger volume to test the player looking at mouse

---
+++ Source/ProjectLambda/Volume/BlendTriggerVolume.cpp
@@ -0,0 +1,82 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlendTriggerVolume.h"
+#include "Components/BoxComponent.h"
+#include "Camera/CameraActor.h"
+#include "ProjectLambda/Player/PlayerCharacter.h"
+#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
+
+// Sets default values
+ABlendTriggerVolume::ABlendTriggerVolume()
+{	
+	//Create box component default components
+	OverlapVolume = CreateDefaultSubobject<UBoxComponent>(TEXT("CameraProximityVolume"));
+
+	//Set the box component attachment to the root component.
+	OverlapVolume->SetupAttachment(RootComponent);
+	
+}
+
+// Called when the game starts or when spawned
+
+void ABlendTriggerVolume::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABlendTriggerVolume::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	const APlayerCharacter* PlayerCharacterCheck = Cast<APlayerCharacter>(OtherActor);
+	
+	if (PlayerCharacterCheck)
+	{
+		APlayerController* PlayerCharacterController = Cast<APlayerController>(PlayerCharacterCheck->GetController());
+
+		if (PlayerCharacterController && Camera)
+		{
+			//Cast check to see if overlapped Actor is Third Person Player Character
+			//Set the actor location and rotation
+
+			bCameraAtLocation = false;
+
+			Camera->SetActorLocation(CameraLocation);
+			Camera->SetActorRotation(CameraRotation);
+
+			//Sets Player Controller view to the first CameraActor found
+			PlayerCharacterController->SetViewTargetWithBlend(Camera, 0.f, EViewTargetBlendFunction::VTBlend_Linear);
+		}
+
+	}
+	
+	
+}
+
+
+
+// Called every frame
+void ABlendTriggerVolume::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//Get camera current location
+	FVector CameraCurrentLocation = Camera->GetActorLocation();
+
+	//If camera is not at new location then we need to move the camera there
+	if (!bCameraAtLocation)
+	{
+		//Basically calculates some stuff to make the camera move there more "smoothly"
+		const FVector NewCameraLocation = FMath::VInterpTo(CameraCurrentLocation, CameraLocation,
+												  GetWorld()->GetDeltaSeconds(), InterpSpeed);
+
+		//Set location with calculation
+		Camera->SetActorLocation(NewCameraLocation);
+			
+		//If camera is at new location set flag to true
+		bCameraAtLocation = CameraCurrentLocation.Equals(CameraLocation);
+		
+	}
+	
+}
+
+
---
+++ Source/ProjectLambda/Volume/BlendTriggerVolume.h
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BlendTriggerVolume.generated.h"
+
+//Source - https://docs.unrealengine.com/5.2/en-US/using-a-static-camera-in-unreal-engine/
+UCLASS()
+class PROJECTLAMBDA_API ABlendTriggerVolume : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+	//Collision Bounds of the Actor Volume
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UBoxComponent* OverlapVolume;
+	
+	UPROPERTY(EditAnywhere, Category="Static Camera", meta=(DisplayName="Static Camera"))
+	AActor* Camera;
+	
+	//Blend time for camera transition
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta =(ClampMin = 0.0f ))
+	float InterpSpeed;
+
+	//Begin and End Overlap methods for our OverlapVolume Actor
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor);
+
+	
+private:
+	UPROPERTY(EditAnywhere, Category="Camera Config", meta=(DisplayName="Camera Location"))
+	FVector CameraLocation;
+
+	UPROPERTY(EditAnywhere, Category="Camera Config", meta=(DisplayName="Camera Rotation"))
+	FRotator CameraRotation;
+
+	bool bCameraAtLocation = true;
+	
+public:
+	// Sets default values for this actor's properties
+	ABlendTriggerVolume();
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
