adaa8a894faefbf44d86355f1b9bf0f56585abd1
Daniel-M40 <daniel.moir0993@gmail.com>
2023-11-04 09:57:05 +0000

    Created PlayerCharacter component

---
+++ Source/ProjectLambda/Player/PlayerCharacter.cpp
@@ -0,0 +1,112 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerCharacter.h"
+
+#include "InputAction.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values
+APlayerCharacter::APlayerCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//Static mesh for the player
+	playerStaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Player Mesh"));
+	playerStaticMesh->SetupAttachment(RootComponent);
+	
+	
+	//Auto posses player when the game starts
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+}
+
+// Called when the game starts or when spawned
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//Get the player controller
+	PlayerController = Cast<APlayerController>(GetController());
+	
+	if (PlayerController)
+	{
+		
+		//Get the subsystem from the player controller
+		UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+
+		//Add the mapping context to the subsystem
+		Subsystem->AddMappingContext(MappingContext, 0);
+	}
+}
+
+// Called every frame
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	if (PlayerController)
+	{		
+		FHitResult HitResult;
+
+		//Get the hit result from the cursor
+		PlayerController->GetHitResultUnderCursor(ECC_Visibility, false, HitResult);
+
+		//DEBUGGER: Draw debug sphere at cursor location
+		if (bIsDebug)
+		{
+			DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, SphereRadius, SphereSegments, FColor::Red);
+		}
+
+		//Rotate the player to where the cursor it located
+		RotatePlayer(HitResult.ImpactPoint, mRotationSpeed);
+
+	}
+}
+
+// Called to bind functionality to input
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+	if (EIC == nullptr)
+		return;
+	
+	EIC->BindAction(MoveInput, ETriggerEvent::Triggered, this, &APlayerCharacter::MoveHandler);
+	EIC->BindAction(StrafeInput, ETriggerEvent::Triggered, this, &APlayerCharacter::StrafeHandler);
+	EIC->BindAction(ShootInput, ETriggerEvent::Triggered, this, &APlayerCharacter::ShootHandler);
+}
+
+
+void APlayerCharacter::RotatePlayer(const FVector& LookAtTarget, const float RotateSpeed)
+{
+	//Get target location Mouse location - Current Location
+	const FVector CurrentLocation = GetActorLocation();
+
+	//Calculate the rotation using the location of the mouse
+	const FRotator TargetRotation(0.f, UKismetMathLibrary::FindLookAtRotation(CurrentLocation, LookAtTarget).Yaw, 0.f);
+		
+	//Update Yaw component
+	SetActorRotation(TargetRotation);
+}
+
+void APlayerCharacter::MoveHandler(const FInputActionValue& Value)
+{
+	AddMovementInput(FVector::ForwardVector * Value.Get<float>());
+}
+
+void APlayerCharacter::StrafeHandler(const FInputActionValue& Value)
+{
+	AddMovementInput(FVector::RightVector * Value.Get<float>());
+}
+
+void APlayerCharacter::ShootHandler(const FInputActionValue& Value)
+{
+	//@@TODO Add shooting functionality
+}
---
+++ Source/ProjectLambda/Player/PlayerCharacter.h
@@ -0,0 +1,73 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class PROJECTLAMBDA_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+private:
+
+	#pragma region Player Config
+	UPROPERTY(EditAnywhere, Category="Player Config", meta=(DisplayName="Player Static Mesh"))
+	class UStaticMeshComponent* playerStaticMesh;
+
+	
+	#pragma endregion
+
+	#pragma region Input Mapping / Actions
+	
+	UPROPERTY(EditAnywhere, Category="Input", meta=(DisplayName="Input Mapping Context"))
+	class UInputMappingContext* MappingContext;
+
+	UPROPERTY(EditAnywhere, Category="Input", meta=(DisplayName="Move Input"))
+	class UInputAction* MoveInput;
+
+	UPROPERTY(EditAnywhere, Category="Input", meta=(DisplayName="Strafe Input"))
+	UInputAction* StrafeInput;
+	
+	UPROPERTY(EditAnywhere, Category="Input", meta=(DisplayName="Fire Input"))
+	UInputAction* ShootInput;
+
+	#pragma endregion
+	
+	class APlayerController* PlayerController;
+
+	UPROPERTY(EditAnywhere, Category="Player Config", meta=(DisplayName="Rotation Speed"))
+	float mRotationSpeed = 10.f;
+
+	UPROPERTY(EditAnywhere, Category="General", meta=(DisplayName="Debug Mode (Logs / Spheres)"))
+	bool bIsDebug = false;
+
+	const float SphereRadius = 25.f;
+	const int SphereSegments = 12;
+	
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void RotatePlayer(const FVector& LookAtTarget, const float RotateSpeed);
+	
+	void MoveHandler(const struct FInputActionValue& Value); //Handles movement forward and backwards
+	
+	void StrafeHandler(const FInputActionValue& Value); //Handles movement left and right
+	
+	void ShootHandler(const FInputActionValue& Value); //Handles the firing input
+	
+};
